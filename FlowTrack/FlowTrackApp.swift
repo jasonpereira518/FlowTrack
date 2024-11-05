5dbf27e1cedec122f5657d424445795a702fe21d
Jason <jnpdude@gmail.com>
2024-11-05 17:25:54 -0500

    Initial Commit

---
+++ FlowTrack/FlowTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowTrackApp.swift
+//  FlowTrack
+//
+//  Created by Jason Pereira on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FlowTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlowTrackTests/FlowTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlowTrackTests.swift
+//  FlowTrackTests
+//
+//  Created by Jason Pereira on 11/5/24.
+//
+
+import Testing
+@testable import FlowTrack
+
+struct FlowTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
